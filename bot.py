7558fda69c032a3a128ac7b1e1547c67ba167d2e
Marie-Claire <mctraore@umd.edu>
2020-01-23 16:10:44 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+import praw
+
+reddit = praw.Reddit(client_id= '',
+                     client_secret= '',
+                     username = '',
+                     password = '',
+                     user_agent = 'Black Positivity Bot')
+
+#subreddit where bot lives
+subreddit = reddit.subreddit('ObsidianTech')
+
+#respond to test thread with "hello world"
+def comment():
+
+    for submission in subreddit.hot(limit = 10):
+        if (submission.title == "Black Positivity Bot Test Thread"):
+            print("Bot commenting hello world.")
+            submission.reply("Hello World!")
+
+comment()

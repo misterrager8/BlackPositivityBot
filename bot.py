4675cfc920cdb74f589cd2e51cb4a51584e5f63c
Marie-Claire <marieclairetraore@gmail.com>
2020-02-05 12:02:07 -0500

    Add post functionality
    All strings use single quotes
    
    Stored string literals into variables

--- bot.py
+++ bot.py
@@ -12,23 +12,32 @@ subreddit = reddit.subreddit('ObsidianTech')
 #keyphrase to activate bot
 keyphrase = '!bpb'
 
-#respond to test thread with "Hello World"
+#respond to specified thread with comment
 def comment():
+    submission_title = 'Black Positivity Bot Test Thread'
+    comment_text = 'Hello World!'
     for submission in subreddit.hot(limit = 10):
-        if (submission.title == "Black Positivity Bot Test Thread"):
-            submission.reply("Hello World!")
-            print("Bot commenting Hello World!")
+        if (submission.title == submission_title):
+            submission.reply(comment_text)
+            print('Bot commenting', comment_text)
 
-#search for keyphrase and respond with "Hello!"
+#search for keyphrase and respond to comment
 def callbot():
+    comment_text = 'Hello!'
     for comment in subreddit.stream.comments():
         try:
             if keyphrase in comment.body and not comment.saved :
-                comment.reply("Hello!")
+                comment.reply(comment_text)
                 comment.save()
-                print("Bot responding to keyphrase with Hello!")
+                print('Bot responding to keyphrase with', comment_text)
         except praw.exceptions.APIException:
             pass
 
+#create a thread in subreddit
+def post_thread():
+    title = 'Bot Posted Test Thread'
+    post_content = 'Black Positivity Bot test'
+    subreddit.submit(title, selftext=post_content)
+    print('"'+ title + '" submission created')
 
-callbot()
+post_thread()

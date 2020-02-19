1e21562645f3a38c97219a7d77a9498714566f06
Marie-Claire <marieclairetraore@gmail.com>
2020-02-18 21:21:44 -0500

    Respond to !inspire with random quote

--- bot.py
+++ bot.py
@@ -6,10 +6,22 @@ reddit = praw.Reddit(client_id= '',
                      client_secret= '',
                      username = '',
                      password = '',
-                     user_agent = '')
+                     user_agent = 'Black Positivity Bot')
 
 #subreddit where bot lives
 subreddit = reddit.subreddit('ObsidianTech')
+
+#http call to API, return random quote and its contributor
+def quote():
+    url = "http://blackpositivityquotes.tk/api/Quotes/random"
+    req = requests.get(url)
+    data = json.loads(req.text)
+    quote_text = data["quote"].strip()
+    if quote_text.startswith('“'):
+        quote_text = quote_text[1:-1]
+    contributor = data["contributor"]
+
+    return '"{}", {}'.format(quote_text, contributor)
 
 #respond to specified thread with comment
 def comment():
@@ -20,31 +32,32 @@ def comment():
             submission.reply(comment_text)
             print('Bot commenting', comment_text)
 
-#search for keyphrase and respond to comment
-def call_bot():
-    keyphrase = '!bpb'
-    comment_text = 'Hello!'
-    for comment in subreddit.stream.comments():
-        try:
-            if keyphrase in comment.body and not comment.saved :
-                comment.reply(comment_text)
-                comment.save()
-                print('Bot responding to keyphrase with', comment_text)
-        except praw.exceptions.APIException:
-            pass
-
 #create a thread in subreddit
 def post_thread():
     title = 'Bot Posted Test Thread'
     post_content = 'Black Positivity Bot test'
     subreddit.submit(title, selftext=post_content)
-    print('"'+ title + '" submission created')
+    print('"{}" submission created'.format(title))
 
-#make http request
-def http_call():
-    url = "http://blackpositivityquotes.tk/api/Quotes/random"
-    r = requests.get(url)
-    data = json.loads(r.text)
-    print(json.dumps(data, indent=2))
+#keyphrases and bot responses
+keyphrases = {'!bpb':'Hello!', '!inspire':quote}
 
-http_call()
+#search for keyphrase and respond to comment
+def call_bot():
+    for comment in subreddit.stream.comments():
+        for phrase in keyphrases:
+            try:
+                if phrase in comment.body and not comment.saved :
+                    if callable(keyphrases[phrase]):
+                        comment_text = keyphrases[phrase]()
+                        comment.reply(comment_text)
+                    else:
+                        comment_text = keyphrases[phrase]
+                        comment.reply(comment_text)
+                    comment.save()
+                    print('Bot responding to', phrase, 'with:', comment_text)
+            except praw.exceptions.APIException as error:
+                print(error)
+                pass
+
+call_bot()

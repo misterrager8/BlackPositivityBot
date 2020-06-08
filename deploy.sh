f69a3f637792564e818590e0f97f9bcdd19647b6
Christopher Smith <csmith4work@gmail.com>
2020-06-08 16:49:10 -0500

    Hopefully this is it

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ echo $_userAgent
 echo $_clientId
 echo $_clientSecret
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 praw_username=$_username praw_user_agent=$_userAgent praw_password=$_password praw_client_secret=$_clientSecret praw_client_id=$_clientId env=$_env python3 bot.py

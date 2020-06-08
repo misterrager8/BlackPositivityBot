b2e9725bb451ae5ac17d440184bb9733c75657dd
Christopher Smith <csmith4work@gmail.com>
2020-06-08 16:32:52 -0500

    Going to implement alternate CICD

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+_env=$1
+_clientId=$2
+_clientSecret=$3
+_password=$4
+_userAgent=$5
+_username=$6
+
+echo $_env
+echo $_username
+echo $_password
+echo $_userAgent
+echo $_clientId
+echo $_clientSecret
+
+pip install -r requirements.txt
+
+praw_username=$_username praw_user_agent=$_userAgent praw_password=$_password praw_client_secret=$_clientSecret praw_client_id=$_clientId env=$_env

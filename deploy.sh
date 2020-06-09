e7d0ceb3e3ad85ab13722eaf4363223eaaf47ad4
Christopher Smith <csmith4work@gmail.com>
2020-06-08 20:23:36 -0500

    Gonna try out pushing container to heroku.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ _clientSecret=$3
 _password=$4
 _userAgent=$5
 _username=$6
+_imagetag=bpb-$_env
 
 echo $_env
 echo $_username
@@ -12,6 +13,15 @@ echo $_userAgent
 echo $_clientId
 echo $_clientSecret
 
-pip3 install -r requirements.txt
+docker build \
+--build-arg env=$_env \
+--build-arg username=$_username \
+--build-arg password=$_password \
+--build-arg userAgent=$_userAgent \
+--build-arg clientId=$_clientId \
+--build-arg clientSecret=$_clientSecret \
+--no-cache -t "$_imagetag" .
 
-praw_username=$_username praw_user_agent=$_userAgent praw_password=$_password praw_client_secret=$_clientSecret praw_client_id=$_clientId env=$_env python3 bot.py
+heroku container:push web
+
+heroku container:release web
